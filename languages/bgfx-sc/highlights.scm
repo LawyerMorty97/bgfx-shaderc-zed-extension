84154ded5ca44c48a725d0e141bafec32dea3ad8
Mathias Berntsen <mathiastb13@gmail.com>
2026-05-26 15:37:28 +0200

    Add shaderc-derived GLSL and bgfx symbols

--- languages/bgfx-sc/highlights.scm
+++ languages/bgfx-sc/highlights.scm
@@ -16,13 +16,13 @@
 
 ; Keywords and builtin types matched safely through identifiers.
 ((identifier) @keyword
-  (#match? @keyword "^(if|else|for|while|do|switch|case|default|break|continue|return|discard|struct|uniform|in|out|inout|const|varying)$"))
+  (#match? @keyword "^(if|else|for|while|do|switch|case|default|break|continue|return|discard|struct|uniform|in|out|inout|const|varying|smooth|flat|centroid|noperspective)$"))
 
 ((type_identifier) @type.builtin
-  (#match? @type.builtin "^(void|bool|int|uint|float|vec2|vec3|vec4|ivec2|ivec3|ivec4|uvec2|uvec3|uvec4|bvec2|bvec3|bvec4|mat2|mat3|mat4|mat2x2|mat2x3|mat2x4|mat3x2|mat3x3|mat3x4|mat4x2|mat4x3|mat4x4|sampler2D|sampler2DArray|sampler2DShadow|samplerCube|sampler3D)$"))
+  (#match? @type.builtin "^(void|bool|int|uint|float|vec2|vec3|vec4|ivec2|ivec3|ivec4|uvec2|uvec3|uvec4|bvec2|bvec3|bvec4|mat2|mat3|mat4|mat2x2|mat2x3|mat2x4|mat3x2|mat3x3|mat3x4|mat4x2|mat4x3|mat4x4|sampler2D|sampler2DArray|sampler2DShadow|samplerCube|sampler3D|sampler2DMS|isampler2D|usampler2D|isampler3D|usampler3D|isamplerCube|usamplerCube|isampler2DMS|usampler2DMS)$"))
 
 ((identifier) @type.builtin
-  (#match? @type.builtin "^(void|bool|int|uint|float|vec2|vec3|vec4|ivec2|ivec3|ivec4|uvec2|uvec3|uvec4|bvec2|bvec3|bvec4|mat2|mat3|mat4|mat2x2|mat2x3|mat2x4|mat3x2|mat3x3|mat3x4|mat4x2|mat4x3|mat4x4|sampler2D|sampler2DArray|sampler2DShadow|samplerCube|sampler3D)$"))
+  (#match? @type.builtin "^(void|bool|int|uint|uint2|uint3|uint4|float|vec2|vec3|vec4|ivec2|ivec3|ivec4|uvec2|uvec3|uvec4|bvec2|bvec3|bvec4|mat2|mat3|mat4|mat2x2|mat2x3|mat2x4|mat3x2|mat3x3|mat3x4|mat4x2|mat4x3|mat4x4|sampler2D|sampler2DArray|sampler2DShadow|samplerCube|sampler3D|sampler2DMS|isampler2D|usampler2D|isampler3D|usampler3D|isamplerCube|usamplerCube|isampler2DMS|usampler2DMS)$"))
 
 [
   "+"
@@ -53,7 +53,7 @@
   (#match? @preproc "^\\$(input|output|raw|end)$"))
 
 ((identifier) @function.builtin
-  (#match? @function.builtin "^(SAMPLER2D|SAMPLER2DARRAY|SAMPLER2DSHADOW|SAMPLERCUBE|SAMPLER3D|IMAGE2D|IMAGE3D|IMAGECUBE|IMAGE2DARRAY|BUFFER_RO|BUFFER_RW|BUFFER_WO|IMAGE_RO|IMAGE_RW|IMAGE_WO|NUM_THREADS|mul|saturate|lerp|vec2_splat|vec3_splat|vec4_splat|mtxFromCols|mtxFromRows|toClipSpaceDepth|encodeNormalUint|decodeNormalUint)$"))
+  (#match? @function.builtin "^(SAMPLER2D|SAMPLER2DARRAY|SAMPLER2DSHADOW|SAMPLERCUBE|SAMPLER3D|IMAGE2D|IMAGE3D|IMAGECUBE|IMAGE2DARRAY|BUFFER_RO|BUFFER_RW|BUFFER_WO|IMAGE_RO|IMAGE_RW|IMAGE_WO|NUM_THREADS|mul|saturate|lerp|vec2_splat|vec3_splat|vec4_splat|mtxFromCols|mtxFromRows|toClipSpaceDepth|encodeNormalUint|decodeNormalUint|texture2D|texture2DProj|texture2DLod|texture2DProjLod|texture2DGrad|texture2DProjGrad|texture2DArray|texture2DArrayLod|texture2DLodOffset|texture3D|texture3DProj|texture3DLod|texture3DProjLod|texture3DGrad|texture3DProjGrad|textureCube|textureCubeLod|textureCubeGrad|shadow2D|shadow2DProj|shadow2DLod|shadow2DProjLod|shadow2DArray|textureSize|texelFetch|texelFetchOffset|dFdx|dFdy|fwidth|bitfieldReverse|floatBitsToInt|floatBitsToUint|intBitsToFloat|uintBitsToFloat|packHalf2x16|unpackHalf2x16)$"))
 
 ((identifier) @constant.builtin
   (#match? @constant.builtin "^(BGFX_SHADER_LANGUAGE_|BGFX_SHADER_TYPE_|BX_PLATFORM_)"))
@@ -68,8 +68,13 @@
 ((identifier) @variable.special
   (#match? @variable.special "^v_[A-Za-z0-9_]+$"))
 
+; shaderc official vertex attributes / instance data inputs
+((identifier) @variable.parameter
+  (#match? @variable.parameter "^(a_position|a_normal|a_tangent|a_bitangent|a_color[0-3]|a_indices|a_weight|a_texcoord[0-7]|i_data[0-4])$"))
+
+; Fallback for project-specific attributes.
 ((identifier) @variable.parameter
   (#match? @variable.parameter "^a_[A-Za-z0-9_]+$"))
 
 ((identifier) @variable.special
-  (#match? @variable.special "^(gl_Position|gl_FragCoord|gl_FragColor|gl_InstanceID|gl_VertexID)$"))
+  (#match? @variable.special "^(gl_Position|gl_FragCoord|gl_FragColor|gl_InstanceID|gl_VertexID|gl_ViewportIndex|gl_Layer)$"))
